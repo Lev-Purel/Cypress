6847598bb82a88bcd5398a667c54d04cd641f284
LevPourel <122628533+LevPourel@users.noreply.github.com>
2025-12-20 14:25:20 +0100

    fead: create separated structure of ui and api tests

--- cypress.config.js
+++ cypress.config.js
@@ -4,8 +4,20 @@ require("dotenv").config({ override: true });
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      watchForFileChanges: true;
+      const specArgIndex = process.argv.indexOf("--spec");
+      if (specArgIndex !== -1) {
+        const spec = process.argv[specArgIndex + 1];
+        if (spec.includes("cypress/e2e/api")) {
+          config.baseUrl =
+            process.env.API_BASE_URL || "https://api.example.com";
+        } else if (spec.includes("cypress/e2e/ui")) {
+          config.baseUrl = "https://www.saucedemo.com/";
+        }
+      }
+      return config;
     },
+    supportFile: "cypress/support/ui/e2e.js",
+    specPattern: "cypress/e2e/ui/**/*.cy.js",
     baseUrl: "https://www.saucedemo.com/",
     testIsolation: true,
   },
